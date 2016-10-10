a56951df9a2e12d3fc0f6efd926ec3c96e7d9e18
JTong <eldertong@gmail.com>
2016-10-10 15:47:15 -0400

    working on bootstrap names

--- app.rb
+++ app.rb
@@ -7,3 +7,33 @@ get '/' do
 @title = 'Home Page'
  erb :index
 end
+
+get '/about' do
+@title = 'About'
+ erb :about
+end
+
+get '/contact' do
+@title = 'Contact'
+ erb :contact
+end
+
+get '/faq' do
+@title = 'FAQ'
+ erb :faq
+end
+
+get '/pricing' do
+@title = 'Pricing'
+ erb :pricing
+end
+
+get '/services' do
+@title = 'Services'
+ erb :services
+end
+
+get '/sidebar' do
+@title = 'Sidebar'
+ erb :sidebar
+end
